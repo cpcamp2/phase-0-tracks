a12c51939140d0e888ac948914e2d4dcfae0c97a
Chris <cpcamp2@gmail.com>
2017-07-23 23:35:14 -0400

    Create questionaire w/ input requests for Release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+employee_name = gets.chomp
+
+puts "How old are you? (ex. 32)"
+employee_age = gets.chomp
+
+puts "What year were you born? (ex. 1922)"
+employee_dob = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+employee_garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+employee_insurance = gets.chomp
