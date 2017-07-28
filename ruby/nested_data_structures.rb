8d9b594960244e23af342b12fb455d1277791e15
Chris <cpcamp2@gmail.com>
2017-07-27 21:02:36 -0400

    Complete assignment

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,24 @@
+classroom = {
+    desk_1: {
+        owner_name: 'Penn',
+        items: [
+			'pencil', 'paper', 'leftover sandwich', 'book'
+		]
+    },
+    desk_2: {
+        owner_name: 'Drew',
+        items: [
+			'pen', 'pencils', 'dominos pizza', 'fish', 'carrot', 'book', 'rings'
+	  	]
+    },
+    desk_3: {
+        owner_name: 'Nat',
+        items: [
+			'pen', 'pencils', 'dominos pizza', 'fish', 'carrot', 'book', 'rings'
+		]
+    },
+}
+
+#classroom[:desk_1][:items][3]
+
+#classroom[:desk_3][:owner_name]
