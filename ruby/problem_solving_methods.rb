cc44d1824f3ef807ac8b16c61ebf7965d6f9b516
Chris <cpcamp2@gmail.com>
2017-07-28 17:41:25 -0400

    Release 2

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -18,6 +18,7 @@ end
 end
 #random_array = [42, 89, 23, 1]
 #p search_array(random_array, 50)
+
 #Release 1
 #Create a method that takes a number of Fibonacci terms to generate and return an array of the terms.
 #Create a method called fib(i) that takes one parameter in this case an integer that represents the number of times we are running the sequence ie the number we want to Fibonacci
@@ -69,13 +70,38 @@ random_array = [42, 89, 23, 1]
 
 p search_array(random_array, 50)
 
-#Release 1
-#Create a method that takes a number of Fibonacci terms to generate and return an array of the terms.
-#Create a method called fib(int) that takes one parameter in this case an integer. 
-def fib(int)
-#Create an array that already has 0 as the first element, within the method definition so that it starts with a fixed value. 
-fib_array = [0]
-#Starting of fib sequence before loop. Where a = 0 and b = 1 
+#Release 2
+#I'll focus on Bubble sort
+#"Bubble sort walks through a list and compares each pair of adjacent items and swaps them if they are in the wrong order" (wikipedia)
+#First Pass
+#( 5 1 4 2 8 ) =>  ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1.
+#( 1 5 4 2 8 ) =>  ( 1 4 5 2 8 ), Swap since 5 > 4
+#( 1 4 5 2 8 ) =>  ( 1 4 2 5 8 ), Swap since 5 > 2
+#( 1 4 2 5 8 ) =>  ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
+#I feel a little bit confused, but feel like once I work through the sorting algorithm it'll make more sense
+#Sitepoint has a great page on sorting-algorithms in ruby.
+#define the bubble sort method with an array (list) to sort through
+def bubble_sort(arr)
+#find the length of the list
+n = arr.length + 1
+#Set up a loop that will compare what is in the list
+loop do
+	# Create a variable called swapped, which is true if any swaps were made while walking down the array
+	#If at the end of the loop swapped is false then return the sorted list.
+	swapped = false
+	(n-1).times do |i|
+		if arr[i] > arr[i + 1]
+			arr[i], arr[i + 1] = arr[i + 1], arr[i]
+			#swapped = true is the termination condition for the loop
+			swapped = true
+		end
+	end
+	break if not swapped
+end
+arr
 
-#Create a while loop that repeats the following sequence a (the first # in the seq), b (the second # in the seq), and 
-# c is the fibonacci number ( a + b ). 
+end
+
+array = [9,1,6,4,3,5]
+
+p bubble_sort(array)
