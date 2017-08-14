09c2c4aa44e80cdde0882398bc1caa5f7dc9820f
Chris <cpcamp2@gmail.com>
2017-08-13 20:32:33 -0400

    Re-did assignment for practice. Reflected and worked on the feedback I got from the last time.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,60 +1,78 @@
-#Create a method that swaps first and last names
-def swap(name)
-  name.split(' ').reverse.join(' ')
-end #method end
-
-
-#test swap(name) method
-#name = gets.chomp
-#swap(name)
-
-#Create a method that changes all vowels to the next vowel and a method that cahnges all consonants to the next consonant.
-def name_alias(name)
-  vowels = ["a","e","i","o","u"]
-  consonants = ["b","c","d","e","f","g","h","j","k","l","m","p","q","r","s","t","v","w","x","y","z"]
-  
-  #create alias w/ swap name
-  next_alias = swap(name.downcase)
-  #split 
-  split_arr = next_alias.split('')
-  #Iterate characters
-  char_arr = split_arr.map do |char|
-    #Loop to see if they contain vowels and consonants and if they do get the next index item for both
-    #Check to see if any of the chars match the chars in vowels
-    if vowels.include?(char)
-      vowels.rotate(1)[vowels.index(char)]
-      
-      #Check to see if any of the chars match the chars in consonants
-      elsif consonants.include?(char)
-      consonants.rotate(1)[consonants.index(char)]
-    else
-        char
-    end #If loop end
-  end #Do loop
-  #Figure out how to capitalize both words
-      puts "Your name is #{name.split.map(&:capitalize).join(' ')}"
-      spy_name = char_arr.join.split.map(&:capitalize).join(' ') 
-      p spy_name
-end #method end
+#Pseudocode and write a method that takes a spy's real name and creates a fakename with it by doing the following:
+#Swapping the first and last name
+#Define name swapper method with a single parameter being the full name
+  #input would be the full name string
+  #split string by the space then reverse and then join with " " between the names
+def name_swapper(name)
+  name.split(" ").reverse.join(" ")
+end#def
 
 #Test method
-#name_alias('felicia torres')
+#p name_swapper("Chris Camp")
 
-#create interface to run through and print name and spy names until they quit and show the key w/ values 
-def interface()
-  #create a hash database to store names and generated spy names
- names_database = {}
- #create a loop that allows user to keep entering names until they type quit
-  loop do
-   puts "Enter your name to generate a spy name. Enter 'quit' to exit."
-    name = gets.chomp.downcase
-    #break if they type in 'quit'
-    break if name == "quit"
-    names_database[name.to_sym] = name_alias(name)
-  end #do loop
-  #print data base key and values for record
-    names_database.each {|key, value| p "#{key.capitalize} has been assigned the name spyname #{value}"}
-    puts "Thank you for using the Spy Name Generator!"
-end #interface
+#Changing all of the vowels to the next vowel
+def next_vowel(char)
+  #Define vowels and create a cipher hash so that the key has a next vowel value
+  vowel_cipher = {'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a'}
+  #if the character has the key then replace the character with that key's value which is the next vowel.
+  if vowel_cipher.has_key?(char)
+    char = vowel_cipher[char]
+  end#if
+end#def
 
-interface()
+#Test method
+#p next_vowel('a')
+
+#Changing all of the consonants to the next consonant
+def next_consonant(char)
+  #Define vowels and create a cipher hash so that the key has a next vowel value
+  consonant_cipher = { 'b' => 'c', 'c' => 'd', 'd' => 'f', 'f' => 'g', 'g' => 'h', 'h' => 'j', 'j' => 'k', 'k' => 'l', 'l' => 'm', 'm' => 'n', 'n' => 'p', 'p' => 'q', 'q' => 'r', 'r' => 's', 's' => 't', 't' => 'v', 'v' => 'w', 'w' => 'x', 'x' => 'y', 'y' => 'z', 'z' => 'b'}
+  #if the character has the key then replace the character with that key's value which is the next consonant
+  if consonant_cipher.has_key?(char)
+    char = consonant_cipher[char]
+  end#if
+end#def
+
+#Test method
+#p next_consonant('b')
+
+#Create an alias
+def create_alias(name)
+  vowels = ["a","e","i","o","u"]
+    consonants = ["b","c","d","e","f","g","h","j","k","l","m","p","q","r","s","t","v","w","x","y","z"]
+#Store the swapped name
+  name_alias = name_swapper(name.downcase)
+#Split string into characters in an array
+  alias_split = name_alias.split('')
+#Iterate through all of the characters
+  new_alias = alias_split.map do |char|
+  #Run a loop to change vowels and consonants into the next vowel using next methods
+    if vowels.include?(char)
+      next_vowel(char)
+    elsif consonants.include?(char)
+      next_consonant(char)
+    else
+      char
+    end#if
+  end#do
+  spy_name = new_alias.join.split.map(&:capitalize).join(' ')
+  #p spy_name
+end#alias
+
+#Test method
+#p create_alias("Felicia Torres")
+
+#User Interface
+database = {}
+puts "Welcome to the spy name generator"
+loop do
+puts "Please enter your name to generate a spy name (type 'q' to quit)"
+name = gets.chomp
+break if name == 'q'
+create_alias(name)
+database[name.to_sym] = create_alias(name)
+end#do
+database.each do |realname, spyname|
+  p "#{realname} has been assigned the spy name of #{spyname}"
+end#do
+puts "Thank you for using the Spy Name Generator"
