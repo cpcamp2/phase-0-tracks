b4f2fb78b681beb9efe43dd4cd18a8570070fb5d
Chris <cpcamp2@gmail.com>
2017-08-14 02:56:26 -0400

    Initial commit

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+// Write a function that takes an array of words or phrases and returns the longest word or phrase in the array
+// First step make an array 
+var myArray = ["long phrase","longest phrase","longer phrase"]
+
+// Create the function w/ a variable that holds the longest string
+function longestWord(myArray) {
+var longestString = " "
+
+// Iterate through 
+
+}
