e882162cc486982df00750ceb129d549f1f9408e
Chris <cpcamp2@gmail.com>
2017-08-25 18:26:12 -0400

    Just playing around with jQuery. It was fun

---
+++ web_dev/jquery/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(green).fadeOut(1000).slideDown('slow');
+    $(blue).fadeOut(1000).slideDown('slow');
+    $(red).fadeOut(1000).slideDown('slow');
+    $(message).slideUp('slow');
+});
