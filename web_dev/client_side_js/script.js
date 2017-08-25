7ceceb297f4f36efe82936ee97f2eb5621256bf3
Chris <cpcamp2@gmail.com>
2017-08-25 12:10:45 -0400

    JS adds redborder

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,10 @@
+// Make sure this js is linked to the html w/ a test
 console.log("Hello world");
+
+function addBorder(event) {
+	console.log(event);
+	event.target.style.border = "5px solid red";
+}
+
+var photo = document.getElementById("stock-photo");
+photo.addEventListener("click", addBorder);
