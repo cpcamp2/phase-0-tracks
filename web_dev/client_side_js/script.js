9e7e0fba57ea1b0e7ebc7607ab6f6d938d03fa8f
Chris <cpcamp2@gmail.com>
2017-08-25 12:27:44 -0400

    Assignment complete

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,10 +1,18 @@
-// Make sure this js is linked to the html w/ a test
-console.log("Hello world");
+// // Make sure this js is linked to the html w/ a test
+// console.log("Hello world");
 
 function addBorder(event) {
 	console.log(event);
 	event.target.style.border = "5px solid red";
 }
 
+//*Tried and failed at getting the image to turn into a gif of a dragon melting ppl
+
+// function changeImage(img) {
+// 	if(img.src.match(/blank/)) img.src = "black.jpg";
+//    else img.src = "blank.jpg";
+// }
+
 var photo = document.getElementById("stock-photo");
 photo.addEventListener("click", addBorder);
+//photo.addEventListener("click", addBorder);
