3ee32da8025ad405b63df7b8cf26b6699724b313
Chris <cpcamp2@gmail.com>
2017-08-26 10:16:03 -0400

    release 1 complete

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,33 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+#Release 0
+
+#I. Add a /contact route that displays an address
+get '/contact' do
+  "114 Monte Vista Ave <br> Charlottesville, VA 22903"
+end
+
+#II. Add a /great_job route that can take a person's name as a query parameter and say "Good job, (name)" and if the query parameter is not present have it just say "Good job!"
+get '/great_job/:person' do
+  person = params[:person]
+  if person
+     "Good job, #{person}!"
+  else
+     "Good job!"
+  end
+end
+
+#III. A route that uses route parameters to add two numbers and respond with the result. Note: data types are tricky when will the data need to arrive as a string?
+get '/add/:number_1/:number_2' do
+  number_1 = params[:number_1].to_i
+  number_2 = params[:number_2].to_i
+  number_3 = number_1 + number_2
+  "#{number_1} + #{number_2} = #{number_3}"
+end
+
+#Release 1
+#I. Sinatra definitely isn't the only ruby framework. Examples of ruby frameworks are: Rails, Padrino, Camping, Remaze, NYNY, Nancy, and so many more!
+#II. There are plenty of other options for using a database with Sinatra. Pretty much any database gem availabe to Ruby should work with sinatra.
+#III. A "web stack" is the collection of software required for Web development. E.g. components, technologies, languages, OS(s), that are used to build a website.
